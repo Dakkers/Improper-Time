e6f1668216834ac9be775e9774c00d4c2bc30478
StDako <d.h.stlaurent@gmail.com>
2013-09-21 02:09:58 -0400

    added a floor! swagariffic

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pyglet as pyg
 from pyglet.window import key
 import pymunk as pym
+from pymunk.pyglet_util import draw as pdraw
 import resources
 
 window = pyg.window.Window(800,400)
+space = pym.Space()
+
+floor = pym.Segment(space.static_body, (0,10), (500,10), 5)
+space.add(floor)
+
 
 mario = pyg.resource.image('mario.png')
 bg = pyg.sprite.Sprite(mario)
+
+#Sprite has height and width attributes - use that for scrolling
 
 
 
@@ -14,7 +22,13 @@ bg = pyg.sprite.Sprite(mario)
 def on_draw():
 	window.clear()
 	bg.draw()
+	pdraw(space)
 
+def update(dt):
+	#print 'derp'
+	space.step(dt)
+
+pyg.clock.schedule_interval(update, 1/10.0)
 
 if __name__ == '__main__':
 	pyg.app.run()
