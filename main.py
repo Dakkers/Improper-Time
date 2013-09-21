642b3c27cee193ecb80abfc993a280d8f03ef0e4
StDako <d.h.stlaurent@gmail.com>
2013-09-21 01:32:49 -0400

    added resources

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pyglet as pyg
 from pyglet.window import key
 import pymunk as pym
+import resources
 
 window = pyg.window.Window(800,400)
+
+mario = pyg.resource.image('mario.png')
+bg = pyg.sprite.Sprite(mario)
 
 
 
 @window.event
 def on_draw():
 	window.clear()
+	bg.draw()
 
 
 if __name__ == '__main__':
---
+++ resources.py
@@ -0,0 +1,5 @@
+import pyglet as pyg
+
+pyg.resource.path = ['resources/']
+pyg.resource.reindex()
+#why the fuck is this here? by habit, actually
